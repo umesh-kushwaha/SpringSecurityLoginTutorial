9d68e038ca0a8cfee58aa84fc247f0e7d31458b8
umesh-kushwaha <umesh220388@gmail.com>
2018-08-25 14:23:38 +0530

    postgresql added and spring version upgraded

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -19,7 +19,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.Transient;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_tbl")
 public class User {
 
 	@Id
